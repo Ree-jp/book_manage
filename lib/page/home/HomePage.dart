0922dc7cc52ef7d1690369260982392adcee8475
Ree_ <mail@ree-jp.net>
2021-01-03 00:26:00 +0900

    bubble bottom barを導入してみた 色が決まらない

---
+++ lib/page/home/HomePage.dart
@@ -0,0 +1,78 @@
+import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("aaa"),
+        elevation: 0,
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.search),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0,-1),
+              blurRadius: 50,
+              color: Theme.of(context).shadowColor.withOpacity(0.2),
+            ),
+          ],
+        ),
+        child: BubbleBottomBar(
+          backgroundColor: Theme.of(context).bottomAppBarColor,
+          opacity: .2,
+          currentIndex: 0,
+          onTap: (int) {},
+          borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+          elevation: 8,
+          fabLocation: BubbleBottomBarFabLocation.end,
+          hasNotch: false,
+          hasInk: true,
+          items: <BubbleBottomBarItem>[
+            BubbleBottomBarItem(
+                backgroundColor: Colors.red,
+                icon: Icon(
+                  Icons.dashboard,
+                  color: Colors.black,
+                ),
+                activeIcon: Icon(
+                  Icons.dashboard,
+                  color: Colors.red,
+                ),
+                title: Text("ホーム")),
+            BubbleBottomBarItem(
+                backgroundColor: Colors.deepPurple,
+                icon: Icon(
+                  Icons.access_time,
+                  color: Colors.black,
+                ),
+                activeIcon: Icon(
+                  Icons.access_time,
+                  color: Colors.deepPurple,
+                ),
+                title: Text("履歴")),
+            BubbleBottomBarItem(
+                backgroundColor: Colors.green,
+                icon: Icon(
+                  Icons.menu,
+                  color: Colors.black,
+                ),
+                activeIcon: Icon(
+                  Icons.menu,
+                  color: Colors.green,
+                ),
+                title: Text("メニュー")),
+          ],
+        ),
+      ),
+    );
+  }
+}
